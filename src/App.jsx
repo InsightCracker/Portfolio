ee1cddaa2dcbc3c91bec08b8103842a3476ad9ff
mking <mkinggrafix@gmail.com>
2026-06-06 21:34:04 -0700

    Fix Bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Navbar from './util/Navbar';
 import LandingPage from './pages/LandingPage';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
-import Product from './pages/Product';
-import Contact from './pages/Contact';
+// import Product from './pages/Product';
+// import Contact from './pages/Contact';
 import SkillContent from './pages/Skillls';
 import Footer from './util/Footer';
 
@@ -15,64 +15,16 @@ function App() {
   aos.init({
     duration: 800
   })
-  const bigCircle = {
-    w: '20rem',
-    h: '20rem',
-    backgroundColor: '#3b82f6',
-    borderRadius: '20px',
-    pos: 'absolute',
-    transform: 'rotate(-45deg)',
-    zIndex: '-1'
-  }
   return (
       <div className="App">
-        <Box className='shapes'>
-          <Box sx={{
-            w: '10rem',
-            h: '10rem',
-            backgroundColor: '#3b82f6',
-            borderRadius: '100%',
-            pos: 'absolute',
-            top: '-50px',
-            left: '-55px'
-          }}></Box>
-
-          <Box sx={{
-            w: '25rem',
-            h: '25rem',
-            backgroundColor: '#3b82f6',
-            borderRadius: '100%',
-            pos: 'absolute',
-            top: '20vh',
-            right: '-270px'
-          }} className='land_circle'></Box>
-
-          <Box sx={{
-            w: '60rem',
-            h: '20rem',
-            backgroundColor: '#3b82f6',
-            borderRadius: '20px',
-            pos: 'absolute',
-            top: '105vh',
-            right: '-65%',
-            transform: 'rotate(-35deg)',
-            zIndex: '-1'
-          }} className='circle'>
-          </Box>
-
-          <Box sx={bigCircle} className='big_circle_a'></Box>
-
-          <Box sx={bigCircle} className='big_circle_b'></Box>
-        </Box>
-
         {/* <Sections /> */}
         <Navbar />
         <LandingPage />
         <About />
         <SkillContent />
         <Portfolio />
-        <Product />
-        <Contact />
+        {/* <Product /> */}
+        {/* <Contact /> */}
         <Footer />
     </div>
   )
